a3e36154e9a616c76f7f5d2ca5ecff0e5aaf3e2f
aizakhurram1 <aiza.khurram@dubizzlelabs.com>
2025-07-11 11:34:58 +0500

    added like and dislikes functionality using livewire component

---
+++ app/Http/Livewire/UpvoteDownvote.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Post;
+use Livewire\Component;
+
+class UpvoteDownvote extends Component
+{
+    public Post $post;
+
+    public function mount(Post $post)
+    {
+        $this->post = $post;
+
+    }
+
+    public function upVoteDownVote($upvote = true)
+    {
+        /** @var \App\Models\User $user */
+        $user = request()->user();
+        if (! $user) {
+            return $this->redirect('login');
+        }
+        if (! $user->hasVerifiedEmail()) {
+            return $this->redirect(route('verification.notice'));
+        }
+        $model = \App\Models\UpvoteDownVote::where('post_id', '=', $this->post->id)
+            ->where('user_id', $user->id)->first();
+
+        if (! $model) {
+            \App\Models\UpvoteDownVote::create([
+                'is_upvote' => $upvote,
+                'post_id' => $this->post->id,
+                'user_id' => $user->id,
+            ]);
+
+            return;
+        }
+        if ($upvote && $model->is_upvote || ! $upvote && ! $model->is_upvote) {
+            $model->delete();
+
+        } else {
+            $model->is_upvote = $upvote;
+            $model->save();
+
+        }
+    }
+
+    public function render()
+    {
+
+        // counting total likes for a single post
+        $upvotes = \App\Models\UpvoteDownVote::where('post_id', '=', $this->post->id)->where('is_upvote', '=', true)->count();
+        $downvotes = \App\Models\UpvoteDownVote::where('post_id', '=', $this->post->id)->where('is_upvote', '=', false)->count();
+
+        $has_up_vote = null; // this will be null if the user hasn't liked or disliked the post
+        /** @var \App\Models\User $user */
+        $user = request()->user();
+
+        if ($user) {
+            $model = \App\Models\UpvoteDownVote::where('post_id', '=', $this->post->id)
+                ->where('user_id', '=', $user->id)
+                ->first();
+
+            if ($model) {
+                $has_up_vote = (bool) $model->is_upvote;
+            }
+        }
+
+        return view('livewire.upvote-downvote', compact('upvotes', 'downvotes', 'has_up_vote'));
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,8 @@
 
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
- <!-- Scripts -->
+     @livewireStyles
+    <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-white font-family-karla">
@@ -148,6 +149,6 @@
     </footer>
 
     
-
+@livewireScripts
 </body>
 </html>
